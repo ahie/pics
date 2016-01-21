a06d88afa69711329b25f761c1692110f0f5636b
ahie <aleksi.hietanen@student.tut.fi>
2016-01-21 15:46:49 +0000

    Hiding config variables from version control

--- Config.php
+++ Config.php
@@ -6,11 +6,11 @@ $config = array(
 
 	'database' => array(
 	        'dsn' => 'pgsql:dbname=picdb;host=localhost',
-	        'username' => 'picuser',
-        	'password' => 'keyboardcat'
+	        'username' => '...',
+        	'password' => '...'
 	),
 
-	'azureConnString' => 'DefaultEndpointsProtocol=http;AccountName=picss;AccountKey=9oGzUNHj+AYCOTobnguwjVIoYidTNsGvpfZAkcDIqkFCgCoBsCLJ1NqZdNwfgFLE99ZxlXKxX1GUDE0hmHfSpQ==',
-	'azureBlobContainer' => 'pics'
+	'azureConnString' => '...',
+	'azureBlobContainer' => '...'
 
 );
